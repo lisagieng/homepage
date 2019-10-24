100ba86cdf559261091529487175b30fe2dc1b05
lisagieng <lisagieng@gmail.com>
2019-10-24 16:33:15 -0400

    uploaded files for page

---
+++ web portfolio/contact.php
@@ -0,0 +1,11 @@
+<!-- contact.php -->
+
+<html>
+    <body>
+
+    Thank you, <?php echo $_POST["inputName"]; ?><br>
+    Your email address is: <?php echo $_POST["inputEmail"]; ?><br>
+    and your inquiry is: <?php echo $_POST["inquiry"]; ?>
+
+    </body>
+</html>
