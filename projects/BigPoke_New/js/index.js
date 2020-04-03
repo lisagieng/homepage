719e095df46d5edb1dc43ce13c57dacc65c951a3
lisagieng <lisagieng@gmail.com>
2020-04-02 21:07:28 -0400

    New project

---
+++ projects/BigPoke_New/js/index.js
@@ -0,0 +1,15 @@
+/*$(document).ready(function(){
+  $("#aboutlink").click(function(){
+    $("#about .box").show(2000);
+  });
+  $("#aboutclose").click(function() {
+    $("#about .box").hide(2000);
+  });
+});
+*/
+
+$(document).ready(function(){
+  $("#about").click(function(){
+    $("#bigpoke").load("test.txt");
+  });
+});
