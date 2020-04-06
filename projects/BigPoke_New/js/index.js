f02b4e8010eabafbc0550904290e5b781ad1ec6b
lisagieng <lisagieng@gmail.com>
2020-04-05 20:24:17 -0400

    Edited JS to toggle about section
    -Updated CSS to have logo to fixed centered position
    -Changed about background color
    -Updated JS to toggle instead of hide/show

--- projects/BigPoke_New/js/index.js
+++ projects/BigPoke_New/js/index.js
@@ -1,15 +1,9 @@
-/*$(document).ready(function(){
-  $("#aboutlink").click(function(){
-    $("#about .box").show(2000);
+$(document).ready(function(){
+  $("#about").click(function() {
+    $(".boxAbout").toggle(2000);
   });
   $("#aboutclose").click(function() {
-    $("#about .box").hide(2000);
+    $("#aboutDiv .boxAbout").hide(2000);
   });
-});
-*/
 
-$(document).ready(function(){
-  $("#about").click(function(){
-    $("#bigpoke").load("about.html");
-  });
 });
