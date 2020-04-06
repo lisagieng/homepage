feba524ee2686bdd5b87db27ab2306090662eaaa
lisagieng <lisagieng@gmail.com>
2020-04-05 20:37:53 -0400

    Added placeholders for rest of menu items
    - Updated JS to work for all 3 divs

--- projects/BigPoke_New/js/index.js
+++ projects/BigPoke_New/js/index.js
@@ -1,9 +1,32 @@
 $(document).ready(function(){
+  //about
   $("#about").click(function() {
-    $(".boxAbout").toggle(2000);
+    $(".boxAbout").toggle("slow");
+    $(".boxContact").hide("slow");
+    $(".boxMenu").hide("slow");
   });
   $("#aboutclose").click(function() {
-    $("#aboutDiv .boxAbout").hide(2000);
+    $("#aboutDiv .boxAbout").hide("slow");
+  });
+
+  //menuDiv
+  $("#menu").click(function() {
+    $(".boxMenu").toggle("slow");
+    $(".boxAbout").hide("slow");
+    $(".boxContact").hide("slow");
+  });
+  $("#menuClose").click(function() {
+    $("#menuDiv .boxMenu").hide("slow");
+  });
+
+  //contact us
+  $("#contact").click(function() {
+    $(".boxContact").toggle("slow");
+    $(".boxAbout").hide("slow");
+    $(".boxMenu").hide("slow");
+  });
+  $("#contactClose").click(function() {
+    $("#contactDiv .boxContact").hide("slow");
   });
 
 });
